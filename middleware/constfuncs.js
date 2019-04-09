9a0687e70c7e6ed048a69969ebe1776874c1c062
AsserHamad <asserhamad96@gmail.com>
2019-04-09 11:28:56 +0200

    walahi msh faker bas 7agat kter

--- middleware/constfuncs.js
+++ middleware/constfuncs.js
@@ -4,57 +4,147 @@ function Variable(name, type, value){
     this.value = value;
 }
 
-let regexAttributes = {
-    declare : '[a-zA-Z0-9]+\s+[a-zA-Z0-9]+(\s+=\s+[a-zA-Z0-9]+)?;',
-    assign : '[a-zA-Z0-9]+(\s+=\s+[a-zA-Z0-9]+)?;'
-};
+function Parameter(name, type){
+    this.name = name;
+    this.type = type;
+}
+
+function Function(name, access, parameters, returnType, static, code){
+    this.name = name;
+    this.access = access;
+    this.parameters = parameters;
+    this.returnType = returnType;
+    this.static = static;
+    this.code = code;
+}
 
 let attributes = [];
+let functions = [];
 
 const constfuncs = (cf) => {
+    cf = cf.replace(/\r|\n/g, '');
     getAttributes(cf);
-    console.log('attributes', attributes);
+    console.log('Attributes List: ', attributes);
     getFunctions(cf);
+    console.log('Functions List: ', functions);
+    //getConstructors(cf);
 }
 
 function getAttributes(cf){
     //Get Properties and Attributes
-    let lines = cf.match(/\s*[a-zA-Z0-9]+\s+[a-zA-Z0-9]+(\s+=\s+[a-zA-Z0-9"]+)?;/g);
-    for (let line of lines) {
-        line = line.substr(0, line.length - 1);
-        line = line.replace(/\r|\n/g, '');
-        line = line.split(' ');
-        let type = line[0],
-            name = line[1],
-            value = (line.length > 2) ? line[3] : null;
-        let variable = new Variable(name, type, value);
-        console.log(`variable`, variable)
-        attributes.push(variable);
-    }
+    let lines = cf.match(/\s*((public|protected|default|private)\s+)?(int|String|double|float)\s+[a-zA-Z0-9]+(\s+=\s+[a-zA-Z0-9"]+)?;/g);
+    if(lines != null)
+        for (let line of lines) {
+            line = line.substr(0, line.length - 1);
+            line = line.split(' ');
+            let type = line[0],
+                name = line[1],
+                value = (line.length > 2) ? line[3] : null;
+            let variable = new Variable(name, type, value);
+            console.log(`New Variable`, variable)
+            attributes.push(variable);
+        }
 }
 
 function getFunctions(cf){
-    console.log('getting indices woo')
+    console.log('////////////////////////Getting Function Indices////////////////////////')
     //Get Functions
-    let indices = indicesOf(cf, 'public|private|default|protected');
-    console.log('indices', indices);
+    let indices = indicesOfFunctions(cf);
+    console.log('Indices of function starts: ', indices);
+    if(indices != null)
+        for(let index of indices){
+            let substr = cf.substr(index, cf.length);
+            console.log('-Substr-', substr);
+            
+            let brackets = [];
+            let end = -1;
+            //To ensure at least one bracket exists
+            let i = 0;
+            while(true){
+                if(substr.charAt(i) == '{'){
+                    brackets.push('{');
+                    i++;
+                    index = i;
+                    break;
+                }
+                i++;
+            }
+            let constr = substr.substr(0, i-1);
+            let funcElements = dissectFunctionConstructor(constr);
+            while(i < substr.length){
+                if(substr.charAt(i) == '{'){
+                    brackets.push('{');
+                }
+                else if(substr.charAt(i) == '}'){
+                    brackets.pop();
+                }
+                if(brackets.length == 0){
+                    end = i;
+                    break;
+                }
+                i++;
+            }
+            let code = (index != end) ? substr.substr(index, end-1) : '';
+            // let func = new Function(funcElements.name, funcElements.access, , funcElements.returnType, funcElements.static, code)
+        }
 }
 
-function indicesOf(string, regex) {
-    let indices = [];
-    let i = 0;
-    regex = new RegExp(regex);
-    console.log('regex', regex);
-    while(true){
-        let index = string.indexOf(regex, i);
-        if(i != 0)
-            indices.push(index);
-        else
-            break;
-        i = index + 1;
+//Function Stuff
+function indicesOfFunctions(string) {
+    let indices = string.match(/(public|private|default|protected) +(static +)?(int|String|double|float|void)([])? +[a-zA-Z_][a-zA-Z0-9]*/g);
+    if(indices != null)
+        for(let i = 0; i < indices.length; i++){
+            indices[i] = string.indexOf(indices[i]);
+        }
+    return indices;
+}
+
+//public static void main(String[] args)
+function dissectFunctionConstructor(string){
+    let phrases = string.match(/(public|private|default|protected) +(static +)?(int|String|double|float|void)([])? +[a-zA-Z_][a-zA-Z0-9]*/)[0].split(' ');
+    let access = 'public', static = false, returnType, name;
+    let parameters = getParameters(string);
+    phrases.map((phrase) => {
+        (phrase.match(/public|private|default|protected/i)) ? access = phrase : null;
+        (phrase.match(/static/i)) ? static = true : null;
+        (phrase.match(/int|String|double|float|void/i)) ? returnType = phrase : returnType;
+    })
+    name = phrases[phrases.length-1];
+    console.log(`access: ${access}, static: ${static}, returnType: ${returnType}, name: ${name}`);
+
+    return {
+        access,
+        static,
+        returnType,
+        name,
+        parameters
     }
 
-    return indices;
+}
+
+function getParameters(string){
+    let params = string.match(/\(.+\)/)[0];
+    params = params.match(/[a-zA-Z _0-9]+/)[0];
+    let returnParams = [];
+    params = params.split(',');
+    params.map((param) => {
+        param = param.split(' ');
+        returnParams.push({
+            type: param[0],
+            name: param[1]
+        })
+    });
+    return returnParams;
+}
+
+//Constructor Work
+function indicesOfConstructors(string){
+    let indices = string.match(/\s+(public|private|default|protected) +[a-zA-Z_][a-zA-Z0-9]*/g);
+    if(indices != null)
+        for(let i = 0; i < indices.length; i++){
+            indices[i] = string.indexOf(indices[i]);
+        }
+        return indices;
 }
 
 module.exports = constfuncs;
--- routes/main.js
+++ routes/main.js
@@ -14,6 +14,7 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
+    console.log(req.body);
     const code = req.body.main;
     const cf = req.body.constfuncs;
 
