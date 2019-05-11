c48ac4c47adb6966de0e332a6136f0195baa8754
Asser Hamad <asserhamad96@gmail.com>
2019-05-11 17:26:33 +0200

    working great so far

--- middleware/main.js
+++ middleware/main.js
@@ -17,34 +17,40 @@ function Sequence(type, details){
 
 const Main = (main, atts, consts, funcs) => {
     attributes = atts, constructors = consts, functions = funcs;
-    handleLogic(main);
-    let ifs = main.match(/if\(.+\){.*}/g);
-    let whiles = main.match(/while\(.+\){.*}/g);
-    let fors = main.match(/for\(.+\){.*}/g);
-    let lines = main.split(';');
-    
-    // let count = 0;
-    // for (let line of lines) {
-    //     count++;
-    //     switch (getTypeOfQuery(line)){
-    //         case 'declaration': declarationHandler(line); break;
-    //         case 'func': functionHandler(line); break;
-    //         case 'staticFunc': staticFunctionHandler(line);break;
-    //         case 'assignment': assignmentHandler(line); break;
-    //     }
-    // }
+    handleLogic(main, sequence);
+    console.log(JSON.stringify(sequence, null, 4));
     return {
-        variables
+        variables,
+        sequence,
+        atts,
+        consts,
+        funcs
     }
 };
 
-function handleLogic(statement){
-    let ifs = statement.match(/if\([a-zA-Z0-9+-=\(\) !]*\)/g);
-    for(let _if of ifs){
-        console.log('---------------------------------------IF-----------------------------------------')
-        console.log(_if);
+function handleLogic(statement, sequence){
+    //Declarations
+    if(statement.match(/^ *(int|String|double|float|Test) [a-zA-Z0-9_] = [a-zA-Z0-9_ .\(\)]+;/i)){
+        let declaration = statement.match(/(int|String|double|float|Test) [a-zA-Z0-9_] = [a-zA-Z0-9_ .\(\)]+;/i)[0]
+        sequence.push({
+            type: 'declaration',
+            declaration
+        });
+        statement = statement.replace(declaration, '');
+    }
+    //Assignments
+    else if(statement.match(/^ *[a-zA-Z0-9_] = [a-zA-Z0-9_ +-/*"'.\(\)]+;/i)){
+        let assignment = statement.match(/[a-zA-Z0-9_] = [a-zA-Z0-9_ +-/*"'.\(\)]+;/i)[0]
+        sequence.push({
+            type: 'assignment',
+            assignment
+        })
+        statement = statement.replace(assignment, '');
+    }
+    //If Conditions
+    else if(statement.match(/^ *if\([a-zA-Z0-9+-=\(\) !]*\)/i)){
+        let _if = statement.match(/if\([a-zA-Z0-9+-=\(\) !]*\)/i)[0];
         let condition = _if.substring(3, _if.length-1);
-        console.log('condition', condition);
         let startIndex = statement.indexOfEnd(_if), counter = 0, endIndex;
         for(let i = startIndex; i < statement.length; i++){
             if(statement[i] == '{')
@@ -56,15 +62,165 @@ function handleLogic(statement){
                 break;
             }
         }
-        console.log('start index', startIndex, ' endIndex', endIndex);
         let block = statement.substring(startIndex+1, endIndex);
-        console.log('block', block);
-        console.log(statement.replace(`${_if}{${block}}`, `[if,${condition},${block}]`));
+        block = handleLogic(block, []);
+        sequence.push({
+            type: 'if',
+            condition,
+            block
+        });
+        statement = statement.replace(statement.substring(0, endIndex+1), '');
     }
+    //While loop
+    else if(statement.match(/^ *while\([a-zA-Z0-9+-=\(\) !]*\)/i)){
+        let _while = statement.match(/while\([a-zA-Z0-9+-=\(\) !]*\)/i)[0];
+        let condition = _while.substring(6, _while.length-1);
+        let startIndex = statement.indexOfEnd(_while), counter = 0, endIndex;
+        for(let i = startIndex; i < statement.length; i++){
+            if(statement[i] == '{')
+                counter++;
+            else if(statement[i] == '}')
+                counter--;
+            if(counter == 0){
+                endIndex = i;
+                break;
+            }
+        }
+        let block = statement.substring(startIndex+1, endIndex);
+        block = handleLogic(block, []);
+        sequence.push({
+            type: 'while',
+            condition,
+            block
+        });
+        statement = statement.replace(statement.substring(0, endIndex+1), '');
+    }
+    //For loop
+    else if(statement.match(/^ *for\([a-zA-Z0-9+-=\(\) ;!]*\)/i)){
+        let _for = statement.match(/for\([a-zA-Z0-9+-=\(\) !]*\)/i)[0];
+        let condition = _for.substring(4, _for.length-1).split(';');
+        let init = condition[0], cond = condition[1], incr = condition[2];
+        let startIndex = statement.indexOfEnd(_for), counter = 0, endIndex;
+        for(let i = startIndex; i < statement.length; i++){
+            if(statement[i] == '{')
+                counter++;
+            else if(statement[i] == '}')
+                counter--;
+            if(counter == 0){
+                endIndex = i;
+                break;
+            }
+        }
+        let block = statement.substring(startIndex+1, endIndex);
+        block = handleLogic(block, []);
+        sequence.push({
+            type: 'for',
+            init,
+            cond,
+            incr,
+            block
+        });
+        statement = statement.replace(statement.substring(0, endIndex+1), '');
+    }
+
+
+    if(statement.match(/^\s*$/)){
+        return sequence;
+    }
+    else
+        return handleLogic(statement, sequence);
 }
+
+// function handleLogic(statement){
+//     let ifs = statement.match(/if\([a-zA-Z0-9+-=\(\) !]*\)/g);
+//     let _ifs = [];
+//     for(let _if of ifs){
+//         console.log('---------------------------------------IF-----------------------------------------')
+//         console.log(_if);
+//         let condition = _if.substring(3, _if.length-1);
+//         console.log('condition', condition);
+//         let startIndex = statement.indexOfEnd(_if), counter = 0, endIndex;
+//         for(let i = startIndex; i < statement.length; i++){
+//             if(statement[i] == '{')
+//                 counter++;
+//             else if(statement[i] == '}')
+//                 counter--;
+//             if(counter == 0){
+//                 endIndex = i;
+//                 break;
+//             }
+//         }
+//         let block = statement.substring(startIndex+1, endIndex);
+//         console.log('BLOCK:', block);
+//         _ifs.push({
+//             condition,
+//             startIndex,
+//             endIndex,
+//             block
+//         });
+//     }
+    
+//     let whiles = statement.match(/while\([a-zA-Z0-9+-=\(\) !]*\)/g);
+//     let _whiles = [];
+//     for(let _while of whiles){
+//         console.log('---------------------------------------WHILE-----------------------------------------')
+//         console.log(_while);
+//         let condition = _while.substring(3, _while.length-1);
+//         console.log('condition', condition);
+//         let startIndex = statement.indexOfEnd(_while), counter = 0, endIndex;
+//         for(let i = startIndex; i < statement.length; i++){
+//             if(statement[i] == '{')
+//                 counter++;
+//             else if(statement[i] == '}')
+//                 counter--;
+//             if(counter == 0){
+//                 endIndex = i;
+//                 break;
+//             }
+//         }
+//         let block = statement.substring(startIndex+1, endIndex);
+//         console.log('BLOCK:', block);
+//         _whiles.push({
+//             condition,
+//             startIndex,
+//             endIndex,
+//             block
+//         });
+//     }
+    
+//     let fors = statement.match(/for\([a-zA-Z0-9+-=\(\) ;!]*\)/g);
+//     let _fors = [];
+//     for(let _for of fors){
+//         console.log('---------------------------------------FOR-----------------------------------------')
+//         console.log(_for);
+//         let condition = _for.substring(3, _for.length-1);
+//         console.log('condition', condition);
+//         let startIndex = statement.indexOfEnd(_for), counter = 0, endIndex;
+//         for(let i = startIndex; i < statement.length; i++){
+//             if(statement[i] == '{')
+//                 counter++;
+//             else if(statement[i] == '}')
+//                 counter--;
+//             if(counter == 0){
+//                 endIndex = i;
+//                 break;
+//             }
+//         }
+//         let block = statement.substring(startIndex+1, endIndex);
+//         console.log('BLOCK:', block);
+//         _fors.push({
+//             condition,
+//             startIndex,
+//             endIndex,
+//             block
+//         });
+//     }
+//     console.log('_ifs', _ifs);
+// }
 
 //int x = 1;
 //TODO: Split by space with no quotes around it
+
 function declarationHandler(line){
     let snips = line.split(/\s+/);
     if(snips.length > 3){
